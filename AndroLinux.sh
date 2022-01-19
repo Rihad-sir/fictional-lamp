94b35d3f6d2b8f7b1b2d366e8f3e24938f34101b
_wh04m!..⟳⚠️ <88466421+Rihad-sir@users.noreply.github.com>
2022-01-19 06:24:08 +0530

    Update AndroLinux.sh

--- AndroLinux.sh
+++ AndroLinux.sh
@@ -1,4 +1,23 @@
-apt install figlet.
+#installin necessary packages
+apt install figlet
+
 echo Done ✅
+apt-get update -y
+echo update Done ✅
+apt-get upgrade -y
+upgrade Done ✅
+pkg install screenfetch
+screen fetech Done ✅
+clear.
+
+sleep 3
+echo Loading...
+
 figlet AndroLinux.
 echo scripted by Rihad-sir
+echo your current divice
+screenfetch.
+Done ✅
+
+
+
